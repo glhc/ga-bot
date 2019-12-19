d69984e3c4a31c98f0596de43b547b1598bd9730
Patrick Horne <patrickahorne@gmail.com>
2019-12-19 18:26:40 +1100

    Make fallback routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   include Knock::Authenticable
   skip_before_action :verify_authenticity_token
   protect_from_forgery
+    def fallback_index_html
+      render :file => 'public/index.html'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   get '/chatroom/:id' => 'chatroom#read_chatroom'
 
 
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+      !request.xhr? && request.format.html?
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
